fa45417bc4369899886ae07c98d7f9541065589a
Ash000es <ash007.uk@icloud.com>
2020-01-29 14:08:40 +0100

    7th commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // selecting page element 
-const inputField = document.querySelector('#input');
-const submit = document.querySelector('#submit');
-const responseField = document.querySelector('#responseField');
+const inputField = document.getElementById('input');
+const submit = document.getElementById('submit');
+const responseField = document.getElementById('responseField');
+const ul= document.getElementById('list');
+const text= document.getElementById('filmText');
 // information to reach API
 const url= 'https://api.themoviedb.org/3/movie/popular?api_key=3e12504afe592b359b01ff7d91ac166f&language=en-US&page=1'
 const wordQuery = inputField.value;
@@ -20,13 +22,22 @@ fetch('https://api.themoviedb.org/3/movie/popular?api_key=3e12504afe592b359b01ff
     results.map(result=> { 
         console.log(result.poster_path)
         const posterLink= `http://image.tmdb.org/t/p/w185//${result.poster_path}`
-console.log(posterLink);})
-  })
-.then (function displaySuggestions (posterLink){
-  let newItem = document.createElement("img");
-  newItem.src = posterLink;
-  let ul = document.getElementById("imageField");
-  ul.appendChild(newItem) 
-      });
+        const titleLink= `http://image.tmdb.org/t/p/w185//${result.original_title}`
+console.log(posterLink);
+let newItem = document.createElement("img")
+let newItem2 = document.createElement("li")
+let textItem= document.createElement('span')
+newItem.src= 'posterLink'
+textItem.innerHTML= 'titleLink'
+parent.appendChild(child);
+newItem2.appendChild(newItem);
+ul.appendChild(newItem2);
+newItem.appendChild(textItem);
 
-  submit.addEventListener('click', displaySuggestions);
+})
+})
+.catch(function(error) {
+  console.log(error)
+});
+
+ // submit.addEventListener('click', displaySuggestions); 

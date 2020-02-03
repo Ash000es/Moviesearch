2bda079a76f0303d8d052359d940aa7a467937f4
Ash000es <ash007.uk@icloud.com>
2020-02-03 17:27:52 +0100

    sort functions submit

--- index.js
+++ index.js
@@ -1,70 +1,126 @@
-// selecting page element 
-const inputField = document.getElementById('input');
-const submit = document.getElementById('submit');
-const responseField = document.getElementById('responseField');
-const ul= document.getElementById('list');
-const text= document.getElementById('filmText');
-//const wordQuery = inputField.value;
-submit.onclick = () => onSearch();
+// selecting page element
 
-// information to reach API
+const submit = document.getElementById('submit')
+const responseField = document.getElementById('responseField')
+const table = document.getElementById('table')
+const movieTitle = document.getElementById('title')
+const popup = document.getElementById('popup')
+const popupWraper = document.getElementById('popupWraper')
 
+submit.onclick = () => onSearch()
+//sortMyVoteCount.onclick = ()=> sortVoteAverage
 
-// get information 
- 
+// create element in html then attach onclick to it
 
+function sortVotecount(arr) {
+  table.innerHTML = ''
+  arr.sort((a, b) => {
+    return a.vote_count > b.vote_count ? -1 : 1
+    console.log(arr)
+  })
+  return displayResults(arr)
+}
 
-fetch('https://api.themoviedb.org/3/movie/popular?api_key=3e12504afe592b359b01ff7d91ac166f&language=en-US&page=1')  
-.then(function(response) {
-    return response.json();
+function sortVoteAverage(arr) {
+  table.innerHTML = ''
+  arr.sort((a, b) => {
+    return a.vote_average > b.vote_average ? -1 : 1
+    console.log(arr)
+    table.innerHTML = ''
+  })
+  return displayResults(arr)
+}
+
+function releaseDate(arr) {
+  table.innerHTML = ''
+  arr.sort((a, b) => {
+    let newDate = Date.parse(a.release_date)
+    let newDate2 = Date.parse(b.release_date)
+    return newDate > newDate2 ? -1 : 1
+    console.log(arr)
+    table.innerHTML = ''
+  })
+  return displayResults(arr)
+}
+
+// helper function
+function displayResults(arr) {
+  return arr.map(subarr => {
+    console.log(subarr.poster_path)
+    if (!subarr.poster_path) return
+    const posterLink = `http://image.tmdb.org/t/p/w185//${subarr.poster_path}`
+
+    let newItem = document.createElement('img')
+    // newItem.addEventListener('click', () => onClickPoster(subarr.overview))
+    newItem.src = posterLink
+    table.appendChild(newItem)
+
+    const sortMyVoteCount = document.getElementById('sortButton')
+    sortMyVoteCount.addEventListener('click', () => sortVotecount(arr))
+    /*let titleBelow= document.createElement('span');
+titleBelow.innerHTML= titleLink;
+movieTitle.appendChild(titleBelow);*/
+    //  3 spearate array.sort
+    // console.log each
+    // on click
+    // change the release date string to a number using
+    //new Date("2012-01-11").getTime()
+    //const titleLink= `${result.title}`
+
+    // 1. when you click on the poster then log to the console.
+    //2. get the movie overview info
+    //3. create an empty pop up
+    //4. add movie overview info to the pop up
+    // 5. animate the pop up
+    //6 close the pop when clicking outside
+  })
+}
+
+/*function onClickPoster(plot) {
+  // show info
+  popup.innerHTML = plot
+  popupwraper.style.display = 'block'
+  popupwraper.classList.add('animated', 'fadeIn')
+  popup.classList.add('animated', 'fadeIn')
+  // popupwraper.style.display = 'flex'
+  popupwraper.onclick = () => {
+    console.log('hi')
+    popupwraper.style.display = 'none'
+  }
+}*/
+// in display
+
+//add the animated class to the popupwrapper
+//set onclick for the popup wrapper
+
+// get information
+
+fetch(
+  'https://api.themoviedb.org/3/movie/popular?api_key=3e12504afe592b359b01ff7d91ac166f&language=en-US&page=1'
+)
+  .then(function(response) {
+    return response.json()
   })
   .then(function(myJson) {
-    console.log(myJson.results);
-   const {results} =myJson
-   console.log(results);
-   return results.map(result=> { 
-       // console.log(result.poster_path)
-        const posterLink= `http://image.tmdb.org/t/p/w185//${result.poster_path}`
-        const titleLink= `${result.title}`
-let newItem2 = document.createElement("li");
-ul.appendChild(newItem2);
-
-let newItem = document.createElement("img");
-newItem.src= posterLink;
-//let finalTry= document.getElementById('list');
-newItem2.appendChild(newItem);
-
-let textItem= document.createElement('span');
-textItem.innerHTML= titleLink;
-newItem2.appendChild(textItem);
-})
-});
+    // console.log(myJson);
+    const {results} = myJson
+    console.log(results)
+    return displayResults(results)
+  })
 
 function onSearch() {
-  const wordQuery = inputField.value;
+  const inputField = document.getElementById('input')
+  const wordQuery = inputField.value
   fetch(
     `https://api.themoviedb.org/3/search/movie?api_key=3e12504afe592b359b01ff7d91ac166f&language=en-US&query=${wordQuery}&page=1&include_adult=false`
   )
-  .then(function(response) {
-      return response.json();
+    .then(function(response) {
+      return response.json()
     })
-    .then(function (myJson) {
-      const {results} =myJson
-      return results.map(result=> { 
-          // console.log(result.poster_path)
-           const posterLink= `http://image.tmdb.org/t/p/w185//${result.poster_path}`
-           const titleLink= `${result.title}`
-   let newItem2 = document.createElement("li");
-   ul.appendChild(newItem2);
-   
-   let newItem = document.createElement("img");
-   newItem.src= posterLink;
-   //let finalTry= document.getElementById('list');
-   newItem2.appendChild(newItem);
-   
-   let textItem= document.createElement('span');
-   textItem.innerHTML= titleLink;
-   newItem2.appendChild(textItem);
-   })
-  });
-};
+    .then(function(myJson) {
+      const {results} = myJson
+      console.log(results)
+      table.innerHTML = ''
+      return displayResults(results)
+    })
+}
